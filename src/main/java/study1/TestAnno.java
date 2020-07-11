3385c7dc4710b826281764618ed7a47d5e158534
dinghuangkai <dinghuangkai@theduapp.com>
2020-07-11 15:23:57 +0800

    初始化项目

---
+++ src/main/java/study1/TestAnno.java
@@ -0,0 +1,16 @@
+package study1;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author huangkaiding
+ * @date 2020/7/11 3:16 下午
+ */
+public class TestAnno {
+
+
+    @Test
+    public void test1(){
+        System.out.println("123");
+    }
+}
